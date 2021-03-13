02b33d92577224672fce98c0003d25cdcf9677cb
Howard Lam <howardlam546@yahoo.com>
2021-03-13 15:56:21 -0800

    added automatic edhr pdf download functionality

--- main.py
+++ main.py
@@ -1,4 +1,67 @@
+import glob
+import os
+import selenium
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from pathlib import Path
+
+path_to_download_folder = str(os.path.join(Path.home(), "Downloads"))
+chromedriver = os.getcwd() + "\\chromedriver_win32\\chromedriver.exe"
+
+# Urls
+edhrUrl = 'http://ussd-illuminareporting.illumina.com/Reports/Pages/Report.aspx?ItemPath=%2fCamstar+Reports%2fProduction%2feDHR+-+Instrument+-+Detail'
+
+def getLatestDownloadedPDF():
+    listOfFiles = glob.glob((path_to_download_folder + "/*")) # * means all if need specific format then *.csv
+    latestFile = max(listOfFiles, key=os.path.getctime)
+    
+    print(latestFile)
+
+    return latestFile
+
+def getEdhrPDF(instrumentSerialNumber):
+    browser = visit(edhrUrl)
+
+    delay = 3
+
+    # Enter serial number into search box
+    searchBox = browser.find_element_by_id('ctl32_ctl04_ctl03_txtValue')
+    browser.implicitly_wait(delay)
+    searchBox.send_keys(instrumentSerialNumber)
+
+    browser.implicitly_wait(delay)
+
+    # # Click on view report button
+    submitButton = browser.find_element_by_id('ctl32_ctl04_ctl00')
+    submitButton.click()
+
+    browser.implicitly_wait(delay)
+
+    # # Find drop down export menu
+    menu = browser.find_element_by_id('ctl32_ctl05_ctl04_ctl00_Menu')
+    browser.execute_script("arguments[0].style.visibility = 'visible'; arguments[0].style.display = 'block'", menu)
+    browser.implicitly_wait(delay)
+
+    # # Click save eDHR report as PDF
+    pdfButton = menu.find_element_by_xpath("//div//a[@title='PDF']")
+    print(pdfButton.get_attribute('title'))
+    pdfButton.click()
+
+    browser.implicitly_wait(delay)
+
+    browser.close()
+
+    return  getLatestDownloadedPDF()
+
+def visit(url):
+    print("[INFO] Visiting {}".format(url))
+    browser = webdriver.Chrome(executable_path=r'{}'.format(chromedriver))         
+    browser.get(url)
+    
+    return browser
+
 def main():
-    print("a")
+    getEdhrPDF('A01420')
 
 main()
